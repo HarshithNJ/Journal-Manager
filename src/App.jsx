f48a5b788dc6885ee341e20cbdaddcf4622b2c9d
Harshith N J <njharshith122@gmail.com>
2025-04-30 11:01:18 +0530

    Create React Project

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
+import Register from './Routers/Register'
+import Login from './Routers/Login'
+import Navbar from './Routers/Navbar'
+
+const App = () => {
+
+  let routers = createBrowserRouter([
+    {
+      path : "/",
+      element : <Navigate to="/register" replace = {true}/>
+    },
+    {
+      path : "/",
+      element : <Navbar />,
+      children : [
+        {
+          path : "/register",
+          element : <Register></Register>
+        },
+        {
+          path : "/login",
+          element : <Login></Login>
+        }
+      ]
+    }
+  ])
+  return (
+    <div>
+      <RouterProvider router = {routers}></RouterProvider>
+    </div>
+  )
+}
+
+export default App
