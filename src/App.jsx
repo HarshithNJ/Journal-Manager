a7874f3904d616cdc3acfaa4ea26cae04723210b
Harshith N J <njharshith122@gmail.com>
2025-05-02 14:27:11 +0530

    routers update

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,45 @@
 import React from 'react'
-import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
-import Register from './Routers/Register'
-import Login from './Routers/Login'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Navbar from './Routers/Navbar'
+import Register from './Routers/Register'
+import AddJournal from './Routers/AddJournal'
+import ViewJournal from './Routers/ViewJournal'
+import UpdateJournal from './Routers/UpdateJournal'
+import Login from './Routers/Login'
 
-const App = () => {
 
-  let routers = createBrowserRouter([
-    // {
-    //   path : "/",
-    //   element : <Navigate to="/register" replace = {true}/>
-    // },
+let routers = createBrowserRouter([
     {
-      path : "/",
-      element : <Navbar />,
-      children : [
-        {
-          path : "/register",
-          element : <Register></Register>
-        },
-        {
-          path : "/login",
-          element : <Login></Login>
-        }
-      ]
+        path : "/",
+        element : <Navbar />,
+        children : [
+            {
+                path : "/register",
+                element : <Register />
+            },
+            {
+                path : "/login",
+                element : <Login></Login>
+            },
+            {
+                path : "/add",
+                element : <AddJournal />
+            },
+            {
+                path : "/view",
+                element : <ViewJournal />
+            },
+            {
+                path : "/update",
+                element : <UpdateJournal />
+            }
+        ]
     }
-  ])
+])
+const App = () => {
   return (
     <div>
-      <RouterProvider router = {routers}></RouterProvider>
+        <RouterProvider router={routers}></RouterProvider>
     </div>
   )
 }
--- src/Routers/Navbar.jsx
+++ src/Routers/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./styles/navbar.css"
 import logo from './images/logo.png'
-import { Link } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 
 const Navbar = () => {
   return (
@@ -12,14 +12,18 @@ const Navbar = () => {
             <img src={logo} alt="Journal" />
           </div>
           <ul>
-            <li>Journals</li>
-            <li>Add Journal</li>
+            <Link to="/view"><li>Journals</li></Link>
+            <Link to="/add"><li>Add Journal</li></Link>
           </ul>
         </div>
         <div id="btns">
           <Link to="/register"><button>Register</button></Link>
-          <button>Login</button>
+          <Link to="/login"><button>Login</button></Link>
         </div>
+      </div>
+
+      <div className='display'>
+        <Outlet></Outlet>
       </div>
     </div>
   )
